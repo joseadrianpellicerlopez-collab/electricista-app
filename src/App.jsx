fd3549dfbe4fdec95e032965154b4d5997ecd7b3
Jose Adrian <electroclimajoseadrian@gmail.com>
2026-05-29 00:04:53 +0200

    Corrige mayusculas rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Horarios from "./pages/Horarios";
+import Home from "./pages/home";
+import Horarios from "./pages/horarios";
 import Finanzas from "./pages/finanzas";
 import TareasPendientes from "./pages/TareasPendientes";
-import Materiales from "./pages/Materiales";
+import Materiales from "./pages/materiales";
 
 export default function App() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/horarios"
-          element={<Horarios />}
-        />
-
-        <Route
-          path="/finanzas"
-          element={<Finanzas />}
-        />
-
-        <Route
-          path="/tareas"
-          element={<TareasPendientes />}
-        />
-
-        <Route
-          path="/materiales"
-          element={<Materiales />}
-        />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/horarios" element={<Horarios />} />
+        <Route path="/finanzas" element={<Finanzas />} />
+        <Route path="/tareas" element={<TareasPendientes />} />
+        <Route path="/materiales" element={<Materiales />} />
       </Routes>
-
     </BrowserRouter>
-
   );
 }
